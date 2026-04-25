686e5a3521a928b6567b1b49430d0a3b1645fd54
Nazmul Haque Nihal <nazmul-haque-nihal@github.com>
2026-04-26 03:09:47 +0600

    Fix summary length and bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.reversi"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
